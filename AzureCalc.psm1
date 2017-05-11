cf2167b3f6e1d09a2b140c9ff706d4376fc5ed21
Andrei Vernigora <andrei.vernigora@outlook.com>
2017-05-12 01:11:06 +0300

    small formatting

--- AzureCalc.psm1
+++ AzureCalc.psm1
@@ -1,59 +1,59 @@
 ﻿function Get-AzureCalcTier($AzureCalcData = $script:rawdata) {
-  $AzureCalcData.Tiers
+    $AzureCalcData.Tiers
 }
 
 function Get-AzureCalcType($AzureCalcData = $script:rawdata) {
-  $AzureCalcData.Types
+    $AzureCalcData.Types
 }
 
 function Get-AzureCalcSize($AzureCalcData = $script:rawdata) {
-  $AzureCalcData.Sizes
+    $AzureCalcData.Sizes
 }
 
 function Get-AzureCalcSoftwareLicense($AzureCalcData = $script:rawdata) {
-  $AzureCalcData.softwareLicenses
+    $AzureCalcData.softwareLicenses
 }
 
 function Get-AzureCalcRegion($AzureCalcData = $script:rawdata) {
-  $AzureCalcData.regions
+    $AzureCalcData.regions
 }
 
 function Get-AzureCalcData {
-  [CmdletBinding()]
-  param()
-  $request = Invoke-WebRequest -Uri https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/?culture=en-us
-  Set-Variable -Name rawdata -Value  ($request.Content | ConvertFrom-Json) -Option ReadOnly -Scope script
+    [CmdletBinding()]
+    param()
+    $request = Invoke-WebRequest -Uri https://azure.microsoft.com/api/v1/pricing/virtual-machines/calculator/?culture=en-us
+    Set-Variable -Name rawdata -Value  ($request.Content | ConvertFrom-Json) -Option ReadOnly -Scope script
   
-  if ($script:rawdata) {
-    $script:calcdata = @()
-    foreach ($type in Get-AzureCalcType) {
-      foreach ($size in Get-AzureCalcSize) {
-        foreach ($tier in Get-AzureCalcTier) {
-            $indexName = "$($type.slug)-$($size.slug)-$($tier.slug)"
-            if ($script:rawdata.offers.$indexName) {
-            $script:calcdata +=
-              [pscustomobject]@{
-                Cores = $script:rawdata.offers.$indexName.cores
-                Ram = $script:rawdata.offers.$indexName.Ram
-                DiskSize = $script:rawdata.offers.$indexName.DiskSize
-                Prices = $script:rawdata.offers.$indexName.Prices
-                Skus = $script:rawdata.offers.$indexName.Skus
-                Type = $type.slug
-                Size = $size.slug
-                Tier = $tier.slug
-                Index = $indexName
-              }
+    if ($script:rawdata) {
+        $script:calcdata = @()
+        foreach ($type in Get-AzureCalcType) {
+            foreach ($size in Get-AzureCalcSize) {
+                foreach ($tier in Get-AzureCalcTier) {
+                    $indexName = "$($type.slug)-$($size.slug)-$($tier.slug)"
+                    if ($script:rawdata.offers.$indexName) {
+                        $script:calcdata += 
+                        [pscustomobject]@{
+                            Cores    = $script:rawdata.offers.$indexName.cores
+                            Ram      = $script:rawdata.offers.$indexName.Ram
+                            DiskSize = $script:rawdata.offers.$indexName.DiskSize
+                            Prices   = $script:rawdata.offers.$indexName.Prices
+                            Skus     = $script:rawdata.offers.$indexName.Skus
+                            Type     = $type.slug
+                            Size     = $size.slug
+                            Tier     = $tier.slug
+                            Index    = $indexName
+                        }
+                    }
+                }
             }
         }
-      }
     }
-  }
 
 
 }
 
 function Get-AzureCalcPrice {
-  <#
+    <#
       .SYNOPSIS
       This function  is used to analyze Azure Calc data extracted by the REST call 
       .DESCRIPTION
@@ -62,80 +62,80 @@ function Get-AzureCalcPrice {
       Get-AzureCalcData
       Get-AzureCalcPrice -Size A4v2 -Region asia-pacific-southeast, canada-east, us-east, us-west | ft -AutoSize
   #>
-  [cmdletbinding()]
-  param (
-    [Parameter()]
-    [int]$CPU, 
+    [cmdletbinding()]
+    param (
+        [Parameter()]
+        [int]$CPU, 
     
-    [Parameter()]
-    [int]$RAM,
+        [Parameter()]
+        [int]$RAM,
     
-    [Parameter()]
-    [ValidateScript({$_ -in (Get-AzureCalcType).slug})]
-    [string]$Type,
+        [Parameter()]
+        [ValidateScript( {$_ -in (Get-AzureCalcType).slug})]
+        [string]$Type,
     
-    [Parameter()]
-    [ValidateScript({$_ -in (Get-AzureCalcSize).slug})]
-    [string]$Size,
+        [Parameter()]
+        [ValidateScript( {$_ -in (Get-AzureCalcSize).slug})]
+        [string]$Size,
     
-    [Parameter()]
-    [ValidateScript({$_ -in (Get-AzureCalcRegion).slug})]
-    [string[]]$Region,
+        [Parameter()]
+        [ValidateScript( {$_ -in (Get-AzureCalcRegion).slug})]
+        [string[]]$Region,
     
-    [Parameter(mandatory = $false)] 
-    $CalcData =  $script:calcdata
-  )
-  begin{
-    $cpuFilter = {param($objectSet)  $objectSet | ? {$_.Cores -eq $CPU} } 
-    $ramFilter = {param($objectSet)  $objectSet | ? {$_.Ram -eq $RAM} }
-    $typeFilter = {param($objectSet)  $objectSet | ? {$_.Type -eq $Type} }
-    $sizeFilter = {param($objectSet)  $objectSet | ? {$_.Size -eq $Size} }
-    $regionFilter = {param($objectSet)  $objectSet | ? { [bool]$r -OR ($Region | % {$_.Prices.keys -contains $_ }); $r } } 
-  }
-  process{
-    $filters = @()
-    if ($CPU){
-      $filters += $cpuFilter
+        [Parameter(mandatory = $false)] 
+        $CalcData = $script:calcdata
+    )
+    begin {
+        $cpuFilter = {param($objectSet)  $objectSet | ? {$_.Cores -eq $CPU} } 
+        $ramFilter = {param($objectSet)  $objectSet | ? {$_.Ram -eq $RAM} }
+        $typeFilter = {param($objectSet)  $objectSet | ? {$_.Type -eq $Type} }
+        $sizeFilter = {param($objectSet)  $objectSet | ? {$_.Size -eq $Size} }
+        $regionFilter = {param($objectSet)  $objectSet | ? { [bool]$r -OR ($Region | % {$_.Prices.keys -contains $_ }); $r } } 
     }
-    if ($RAM){
-      $filters += $ramFilter
-    }
-    if ($Type){
-      $filters += $typeFilter
-    }
-    if ($Size){
-      $filters += $sizeFilter 
-    }
-    
-    if ($Region){
-      $filters += $regionFilter 
-    }
-    
-    $ret = $CalcData
-    foreach ($f in $filters){
-      $ret = & $f $ret
-    }
-    
-    if ($Region) {
-      foreach ($el in $ret) {
-        $res =  [ordered]@{
-          Index = $el.index
-          Type = $el.type
-          Size = $el.size
-          Cores = $el.Cores
-          Ram = $el.Ram
-          DiskSize = $el.DiskSize          
+    process {
+        $filters = @()
+        if ($CPU) {
+            $filters += $cpuFilter
         }
-          
-        foreach ($r in $Region){
-          $res.$r = $el.Prices.$r
+        if ($RAM) {
+            $filters += $ramFilter
         }
+        if ($Type) {
+            $filters += $typeFilter
+        }
+        if ($Size) {
+            $filters += $sizeFilter 
+        }
+    
+        if ($Region) {
+            $filters += $regionFilter 
+        }
+    
+        $ret = $CalcData
+        foreach ($f in $filters) {
+            $ret = & $f $ret
+        }
+    
+        if ($Region) {
+            foreach ($el in $ret) {
+                $res = [ordered]@{
+                    Index    = $el.index
+                    Type     = $el.type
+                    Size     = $el.size
+                    Cores    = $el.Cores
+                    Ram      = $el.Ram
+                    DiskSize = $el.DiskSize          
+                }
           
-        [pscustomobject] $res
-      }
+                foreach ($r in $Region) {
+                    $res.$r = $el.Prices.$r
+                }
+          
+                [pscustomobject] $res
+            }
+        }
+        else {
+            $ret
+        }
     }
-    else{
-      $ret
-    }
-  }
 }
