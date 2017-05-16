da83132bc7fa546ccb4a2490194e4c8b93363278
Andrei Vernigora <andrei.vernigora@outlook.com>
2017-05-17 02:21:33 +0300

    test updated

--- tests/azurecalc.pester.tests.ps1
+++ tests/azurecalc.pester.tests.ps1
@@ -3,7 +3,7 @@ Import-Module "..\AzureCalc" -verbose
 Describe "AzureCalc tests"{
     Context "extract data from azure"{
         it "Extract data"{
-            Get-AzureCalcData | Should Not Throw
+           { Get-AzureCalcData }  | Should Not Throw
         }
     }
 }
