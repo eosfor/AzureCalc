758b12647f28b94c9bdd7093fed3c48f9f5cc41f
Andrei Vernigora <andrei.vernigora@outlook.com>
2017-05-17 16:09:19 +0300

    interface changed. arrays are supported for CPU/RAM. +more

--- AzureCalc.psm1
+++ AzureCalc.psm1
@@ -42,6 +42,7 @@ function Get-AzureCalcData {
                             Size     = $size.slug
                             Tier     = $tier.slug
                             Index    = $indexName
+                            DisplayName = "$($tier.Displayname)_$($size.Displayname -replace " ", "_")"
                         }
                     }
                 }
@@ -53,122 +54,155 @@ function Get-AzureCalcData {
 }
 
 function Get-AzureCalcPrice {
-    <#
+  <#
       .SYNOPSIS
       This function  is used to analyze Azure Calc data extracted by the REST call 
       .DESCRIPTION
       This function  is used to analyze Azure Calc data extracted by the REST call. It can then filter the data by CPU, RAM, OS Type, VM  Size, Region
+      
       .EXAMPLE
       Get-AzureCalcData
       Get-AzureCalcPrice -Size A4v2 -Region asia-pacific-southeast, canada-east, us-east, us-west | ft -AutoSize
+      
+      .EXAMPLE
       $r = 'us-east', 'us-east-2'
       Get-AzureCalcPrice   -Region $r -CPU 3 -ram 10 -Type windows -Tier standard -ShowClosestMatch | ft -AutoSize
       Get-AzureCalcPrice   -Region $r -CPU 8 -ram 14 -Type windows -Tier standard | ft -AutoSize
+
+      .EXAMPLE
+      Get-AzureCalcData
+      Get-AzureCalcPrice -Region australia-east -Tier standard -Type windows | ft -AutoSize
+      Get-AzureCalcPrice -CPU (8..16) -Region australia-east -Tier standard -Type windows | ft -AutoSize
+      Get-AzureCalcPrice -CPU (8..16) -RAM (20..128) -Region australia-east -Tier standard -Type windows | sort  australia-east | ft -AutoSize
+
+
+      Get-AzureCalcPrice -Size F8 -Region australia-east -Tier standard -Type windows | ft -AutoSize
+      Get-AzureCalcPrice -Size F2 -Region australia-east -Tier standard -Type windows | ft -AutoSize
+      Get-AzureCalcPrice -CPU (8..16) -RAM (20..40) -Region australia-east -Tier standard -Type windows | sort  australia-east | ft -AutoSize
+      Get-AzureCalcPrice -CPU (2..16) -RAM (4..20) -Region australia-east -Tier standard -Type windows | sort  australia-east | ft -AutoSize
+      Get-AzureCalcPrice -CPU (4..8) -RAM (4..32) -Region australia-east -Tier standard -Type windows | sort  australia-east | ft -AutoSize
   #>
-    [cmdletbinding()]
-    param (
-        [Parameter(ParameterSetName = "hardware")]
-        [int]$CPU, 
+  [cmdletbinding(DefaultParameterSetName="Region")]
+  param (
+    [Parameter(ParameterSetName = "hardware")]
+    [int[]]$CPU, 
     
-        [Parameter(ParameterSetName = "hardware")]
-        [int]$RAM,
+    [Parameter(ParameterSetName = "hardware")]
+    [int[]]$RAM,
     
-        [Parameter(ParameterSetName = "hardware")]
-        [ValidateScript( {$_ -in (Get-AzureCalcType).slug})]
-        [string]$Type,
+    [Parameter(ParameterSetName = "hardware")]
+    [Parameter(ParameterSetName = "size")]
+    [Parameter(ParameterSetName = "Region")]
+    [ValidateScript( {$_ -in (Get-AzureCalcType).slug})]
+    [string]$Type,
     
-        [Parameter(ParameterSetName = "size")]
-        [ValidateScript( {$_ -in (Get-AzureCalcSize).slug})]
-        [string]$Size,
+    [Parameter(ParameterSetName = "size")]
+    [ValidateScript( {$_ -in (Get-AzureCalcSize).slug})]
+    [string]$Size,
     
-        [Parameter(ParameterSetName = "hardware")]
-        [Parameter(ParameterSetName = "size")]
-        [ValidateScript( {$_ -in (Get-AzureCalcRegion).slug})]
-        [string[]]$Region,
+    [Parameter(ParameterSetName = "hardware")]
+    [Parameter(ParameterSetName = "size")]
+    [Parameter(ParameterSetName = "Region")]
+    [ValidateScript( {$_ -in (Get-AzureCalcRegion).slug})]
+    [string[]]$Region,
 
-        [Parameter(ParameterSetName = "hardware")]
-        [Parameter(ParameterSetName = "size")]
-        [ValidateScript( {$_ -in (Get-AzureCalcTier).slug})]
-        [string[]]$Tier,
-
-        [Parameter(mandatory = $false)]
-        [Parameter(ParameterSetName = "hardware")]
-        [switch]$ShowClosestMatch,
+    [Parameter(ParameterSetName = "hardware")]
+    [Parameter(ParameterSetName = "size")]
+    [Parameter(ParameterSetName = "Region")]
+    [ValidateScript( {$_ -in (Get-AzureCalcTier).slug})]
+    [string[]]$Tier = "standard",
+   
+    [Parameter(mandatory = $false)]
+    [Parameter(ParameterSetName = "hardware")]
+    [switch]$ShowClosestMatch,
     
-        [Parameter(mandatory = $false)] 
-        [Parameter(ParameterSetName = "hardware")]
-        [Parameter(ParameterSetName = "size")]
-        $CalcData = $script:calcdata
-    )
-    begin {
-        function getClosest ($objectList, $propName, $num) {
-            $diff = [math]::Abs(($num - ($objectList.$propName)[0]))
-            $objectList | ForEach-Object {$r = @{}} {
-                $currObj = $_
-                $curr = $_.$propName
-                $abs = [math]::Abs(($curr - $num))
-                $r.$Abs = $r.$Abs + @($currObj)
-            } { 
-                $minValueKey = ($r.Keys | Sort-Object | Select-Object -First 1)
-                $minByProperty = $r[$minValueKey] | Where-Object "$propname" -le $num
-                $minByProperty
-            }
+    [Parameter(mandatory = $false)] 
+    [Parameter(ParameterSetName = "hardware")]
+    [Parameter(ParameterSetName = "size")]
+    [Parameter(ParameterSetName = "Region")]
+    $CalcData = $script:calcdata
+  )
+  begin {
+    function getClosest ($objectList, $propName, $num) {
+      $diff = [math]::Abs(($num - ($objectList.$propName)[0]))
+      $objectList | ForEach-Object {$r = @{}} {
+        $currObj = $_
+        $curr = $_.$propName
+        $num | ForEach-Object {
+          $abs = [math]::Abs(($_ - $curr))
+          $r.$Abs = $r.$Abs + @($currObj)        
         }
-        $cpuFilter = if (! $ShowClosestMatch.IsPresent) {{param($objectSet)  $objectSet | Where-Object {$_.Cores -eq $CPU} }} 
-        else { {param($objectSet) $t = getClosest $objectSet 'Cores' $CPU; $t} }
-        $ramFilter = if (! $ShowClosestMatch.IsPresent) {{param($objectSet)  $objectSet | Where-Object {$_.Ram -eq $RAM} }}
-        else { {param($objectSet) $t = getClosest $objectSet 'Ram' $RAM; $t}  }
-        $typeFilter = {param($objectSet)  $objectSet | Where-Object {$_.Type -eq $Type} }
-        $sizeFilter = {param($objectSet)  $objectSet | Where-Object {$_.Size -eq $Size} }
-        $tierFilter = {param($objectSet)  $objectSet | Where-Object {$_.Tier -eq $Tier} }
-        $regionFilter = {param($objectSet)  $objectSet | Where-Object { [bool]$r -OR ($Region | ForEach-Object {$_.Prices.keys -contains $_ }); $r } } 
+      } { 
+        $minValueKey = ($r.Keys | Sort-Object | Select-Object -First 1)
+        $minByProperty = $r[$minValueKey] | Where-Object "$propname" -le $num #first try to pick lower value
+        if (! $minByProperty ) {$minByProperty = $r[$minValueKey] | Where-Object "$propname" -ge $num} #if it does not exist picking higher value
+        $minByProperty
+      }
     }
-    process {
-        $filters = @()
-        if ($CPU) {
-            $filters += $cpuFilter
-        }
-        if ($RAM) {
-            $filters += $ramFilter
-        }
-        if ($Type) {
-            $filters += $typeFilter
-        }
-        if ($Size) {
-            $filters += $sizeFilter 
-        }
-        if ($Tier) {
-            $filters += $tierFilter 
-        }
+    $cpuFilter = if (! $ShowClosestMatch.IsPresent) {{param($objectSet)  $objectSet | Where-Object {$_.Cores -in @($CPU)} }} 
+    else { {
+        param($objectSet) 
+        $t = getClosest $objectSet 'Cores' $CPU
+        $t} }
+    $ramFilter = if (! $ShowClosestMatch.IsPresent) {{param($objectSet)  $objectSet | Where-Object {$_.Ram -in @($RAM)} }}
+    else { {
+        param($objectSet) 
+        $t = getClosest $objectSet 'Ram' $RAM 
+        $t}  }
+    $typeFilter = {param($objectSet)  $objectSet | Where-Object {$_.Type -eq $Type} }
+    $sizeFilter = {param($objectSet)  $objectSet | Where-Object {$_.Size -eq $Size} }
+    $tierFilter = {param($objectSet)  $objectSet | Where-Object {$_.Tier -eq $Tier} }
+    $regionFilter = {param($objectSet)  $objectSet | Where-Object { [bool]$r -OR ($Region | ForEach-Object {$_.Prices.keys -contains $_ }); $r } } 
+  }
+  process {
+    $filters = @()
+    if ($Region) {
+      $filters += $regionFilter 
+    }
+
+    if ($CPU) {
+      $filters += $cpuFilter
+    }
+    if ($RAM) {
+      $filters += $ramFilter
+    }
+    if ($Type) {
+      $filters += $typeFilter
+    }
+    if ($Size) {
+      $filters += $sizeFilter 
+    }
+    if ($Tier) {
+      $filters += $tierFilter 
+    }
     
-        if ($Region) {
-            $filters += $regionFilter 
-        }
     
-        $ret = $CalcData
-        foreach ($f in $filters) {
-            $ret = & $f $ret
-        }
+    $ret = $CalcData
+    foreach ($f in $filters) {
+      $ret = & $f $ret
+    }
     
-        if ($Region) {
-            foreach ($el in $ret) {
-                $res = [ordered]@{
-                    Index    = $el.index
-                    Type     = $el.type
-                    Size     = $el.size
-                    Cores    = $el.Cores
-                    Ram      = $el.Ram
-                    DiskSize = $el.DiskSize          
-                }
+    if ($Region) {
+      foreach ($el in $ret) {
+        $res = [ordered]@{
+          Index    = $el.index
+          Type     = $el.type
+          Size     = $el.size
+          Tier     = $el.Tier
+          Cores    = $el.Cores
+          Ram      = $el.Ram
+          DiskSize = $el.DiskSize
+          DisplayName = $el.DisplayName
+        }
           
-                foreach ($r in $Region) {
-                    $res.$r = $el.Prices.$r
-                }
-          
-                [pscustomobject] $res
-            }
+        foreach ($r in $Region) {
+          $res.$r = $el.Prices.$r
         }
-        else {
+          
+        [pscustomobject] $res
+      }
+    }
+    else {
             $ret
         }
     }
