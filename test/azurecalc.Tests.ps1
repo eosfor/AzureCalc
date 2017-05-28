c382e5df71959e930649ac8804dac890523ef74c
Andrei Vernigora <andrei.vernigora@outlook.com>
2017-05-29 01:28:21 +0300

    tests updated (some small set of tests)

--- test/azurecalc.Tests.ps1
+++ test/azurecalc.Tests.ps1
@@ -1,9 +1,13 @@
 $ModuleManifestName = 'azurecalc.psd1'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+$current = Get-Location
 
+# trying to import module
 Import-Module $ModuleManifestPath -Verbose -Force
 
-$outPath = 'E:\temp\awsregions'
+# creating a temporary folder under current location
+$foldername = "awsregions"
+$outPath = join-path -Path $current -ChildPath $foldername
 if (! (Test-Path $outPath)) {mkdir $outPath}
 
 Describe 'Module Manifest Tests' {
@@ -14,15 +18,30 @@ Describe 'Module Manifest Tests' {
 }
 
 Describe 'Module  Tests' {
-    It 'Passes Get-AWSOfferData to a new folder (does not exist)' {
-        { Get-AWSOfferData -Path $outPath } | Should Throw
-    }
-    It 'Passes Get-AWSOfferData to an existing folder with files' {
+    It 'Get-AWSOfferData -Path $outPath -Force -PassThru' {
         $p = Get-AWSOfferData -Path $outPath -Force -PassThru
         (dir $p).count | Should NOT BeNullorEmpty
     }
 
-    It "Passes Import-AWSOfferDataFile" {
+    It "Get-AWSCalcPrice -CPU  8 -RAM  (8..32) -Region 'somenonexistentrgn'"{
+        { Get-AWSCalcPrice -CPU  8 -RAM  (8..32) -Region 'someregion' } | Should Throw "Please import AWS calc data"
+    }
+
+    It "Import-AWSOfferDataFile -Path $outPath" {
         { Import-AWSOfferDataFile -Path $outPath -PassThru } | should NOT Throw
     }
+
+    It "Second import and passthru" {
+        $k = Import-AWSOfferDataFile -Path $outPath -PassThru
+        $k | should NOT BeNullOrEmpty
+        $k | Should BeOfType System.Collections.Hashtable
+    }
+
+    It "Get-AWSCalcPrice -CPU  8 -RAM  (8..32) -Region 'us-east-1'" {
+        { Get-AWSCalcPrice -CPU  8 -RAM  (8..32) -Region 'us-east-1' } | Should Not Throw
+    }
 }
+
+
+#cleaning up
+del $outPath -Force -Recurse
